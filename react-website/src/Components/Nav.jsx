a3b80dea55a87de16da94d4d09e8d4fdaeefdeb4
KP-Nemaranzhe <pronemaranzhe@gmail.com>
2024-07-31 11:51:39 +0100

    add

--- react-website/src/Components/Nav.jsx
+++ react-website/src/Components/Nav.jsx
@@ -3,27 +3,52 @@ const Nav = () => {
     const content = <>
     <div className="">
         <ul>
-            <link>
-            <li>Home</li>
+            <link to="Home">
+                <li>Home</li>
             </link>
-            <link>
-            <li>About</li>
+            <link to="About">
+                <li>About</li>
             </link>
-            <link>
-            <li>Services</li>
+            <link to="Services">
+                <li>Services</li>
             </link>
-            <link>
-            <li>Projects</li>
+            <link to="Projects">
+                <li>Projects</li>
             </link>
-            <link>
-            <li>Contact</li>
+            <link to="Contact">
+                <li>Contact</li>
             </link>
         </ul>   
     </div>
     </>
     return(
         <nav>
-
+            <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 ">
+                <div className="flex items-center flex-1">
+                    <span className="text-3xl font-bold">Logo</span>
+                </div>
+                <div className="lg:flexx md:flex lg: flex-1 items center justify-end font-nor">
+                    <div className="flex-10">
+                    <ul className="flex gap-8 mr-16 text-[18px]">
+                <link to="Home">
+                    <li>Home</li>
+                </link>
+                <link to="About">
+                    <li>About</li>
+                </link>
+                <link to="Services">
+                    <li>Services</li>
+                </link>
+                <link to="Projects">
+                    <li>Projects</li>
+                </link>
+                <link to="Contact">
+                    <li>Contact</li>
+                </link>
+                   </ul>
+                   </div>   
+                </div>
+            </div>
         </nav>
     )
 };
