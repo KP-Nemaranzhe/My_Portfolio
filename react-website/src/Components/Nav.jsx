d6631ca2fad1f01795c515cd9c44aefde793a6ba
KP-Nemaranzhe <pronemaranzhe@gmail.com>
2024-07-31 11:26:33 +0100

    navigation

--- react-website/src/Components/Nav.jsx
+++ react-website/src/Components/Nav.jsx
@@ -1,6 +1,26 @@
 
 const Nav = () => {
-    const content = 
+    const content = <>
+    <div className="">
+        <ul>
+            <link>
+            <li>Home</li>
+            </link>
+            <link>
+            <li>About</li>
+            </link>
+            <link>
+            <li>Services</li>
+            </link>
+            <link>
+            <li>Projects</li>
+            </link>
+            <link>
+            <li>Contact</li>
+            </link>
+        </ul>   
+    </div>
+    </>
     return(
         <nav>
 
