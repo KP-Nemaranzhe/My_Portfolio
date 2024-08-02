844179d4af5a9652788812da408b9ff87ba69573
KP-Nemaranzhe <pronemaranzhe@gmail.com>
2024-08-02 16:51:57 +0100

    new file

---
+++ src/data/experienceData.js
@@ -0,0 +1,17 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'TheWebPatriot',
+        jobtitle: 'Full Stack Developer (MERN Stack)',
+        startYear: 'Oct 2022',
+        endYear: 'Jan 2024'
+    },
+    {
+        id: 2,
+        company: 'Masai School',
+        jobtitle: 'Full Stack Developer (MERN Stack)',
+        startYear: 'Oct 2021',
+        endYear: 'Sep 2022'
+    },
+    
+]
